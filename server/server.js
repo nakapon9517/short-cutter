a6ce2500c39fc0ea0438a5c51afe32029d961388
nakapon9517 <maksminakapon@gmail.com>
2020-05-14 01:00:23 +0900

    Environment

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 import express from "express";
 import fs from "fs";
+import webpack from "webpack";
+import React from "react";
+import { renderToString } from "react-dom/server";
+
+import webpackConfig from "../webpack.config.dev.babel";
+import webpackMiddleware from "webpack-dev-middleware";
+import HMR from "webpack-hot-middleware";
+
+import App from "../src/App";
 
 const app = express();
 
+const compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler));
+app.use(HMR(compiler));
+
 //GETリクエストでルートにアクセスが会った時の動作
 app.get("/", (req, res) => {
-  const index = fs.readFileSync("./public/index.html", "utf-8");
+  // const index = fs.readFileSync("./public/index.html", "utf-8");
+
+  let index = fs.readFileSync("./public/index.html", "utf-8");
+  const appRendered = renderToString(<App />);
+  index = index.replace("<%= preloadedApplication %>", appRendered);
   res.send(index);
 });
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("root"));
+const render = (_App) => {
+  ReactDOM.render(<_App />, document.getElementById("root"));
+};
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    render(NextApp);
+  });
+}
+
+render(App);
